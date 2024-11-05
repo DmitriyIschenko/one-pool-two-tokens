0e7eeb2e22674ffc3559212f144191778a4456ce
Dmitriy Ishchenko <dmitriysimit@gmail.com>
2024-11-05 20:46:05 +0100

    added .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import {configDotenv} from "dotenv";
+import {resolve} from "path";
+configDotenv({ path: resolve(__dirname, "./.env") });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks:{
+    bsc:{
+      url:"https://bsc-pokt.nodies.app",
+      accounts:[process.env.PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
